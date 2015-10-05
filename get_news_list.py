4096d8c0f4462ea89ef3e16ff3221c388177149a
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-10-05 21:48:42 +0530

    Made get_news_dict a method
    This method is reusable and can be imported by other classes of the module.
    Also, included default_offset config value in data.yml. This decides the default
    number of days from which the editorials are to be fetched.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- get_news_list.py
+++ get_news_list.py
@@ -7,29 +7,43 @@ import anyconfig
 from datetime import datetime, timedelta
 import json
 
-# Getting url of the api to call
-config = anyconfig.load('data.yml')
-if config is None or 'baseurl' not in config:
-        print "Error parsing yaml"
-        exit(1)
+def get_news_dict(ts=None):
+        """Returns a dict containing list of editorials after timestamp ts. By
+        default, the difference is mentioned as 'default_offset' in data.yml."""
 
-baseurl = config['baseurl']
-listurl = baseurl + '/api/list'
+        # Getting url of the api to call from data.yml file
+        config = anyconfig.load('data.yml')
+        if config is None or 'baseurl' not in config or 'default_offset' not in config:
+                print "Error parsing yaml"
+                return None
 
-# Getting timestamp of 4 days in past
-now = datetime.now()
-prev = now - timedelta(1)
-ts = (prev - datetime(1970, 1, 1)).total_seconds()
+        baseurl = config['baseurl']
+        listurl = baseurl + '/api/list'
 
-# Adding the timestamp to payload
-payload = {}
-payload['timestamp'] = ts
+        # Getting timestamp of default offset as mentioned in data.yml
+        if ts is None:
+                doff = config['default_offset']
+                now = datetime.now()
+                prev = now - timedelta(doff)
+                ts = (prev - datetime(1970, 1, 1)).total_seconds()
 
-# Sending the request
-resp = requests.post(listurl, data=payload)
-if resp.status_code != 200:
-        print "Error received : " + str(resp.status_code)
-        exit(2)
-else:
-        cntnt = json.loads(resp.content)
-        print json.dumps(cntnt, indent=2, sort_keys=True)
+        # Adding the timestamp to payload
+        payload = {}
+        payload['timestamp'] = ts
+
+        # Sending the request
+        resp = requests.post(listurl, data=payload)
+
+        # Analysing the response
+        if resp.status_code != 200:
+                print "Error received : " + str(resp.status_code)
+                return None
+        else:
+                cntnt = json.loads(resp.content)
+                return cntnt
+
+if __name__ == '__main__':
+        news_dict = get_news_dict()
+        if news_dict is not None:
+                # Pretty print the dictionary received
+                print json.dumps(news_dict, indent=2, sort_keys=True)
