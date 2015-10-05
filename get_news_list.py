8f3ed279e445aeae307093b1605cde5abeae858a
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-10-05 08:08:18 +0530

    Rudimentary script for getting news list
    Script gets the editorials published in the last 4 days. The json received is
    printed as is on the screen.
    
    This process is the first step toward getting the editorials from the server.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- get_news_list.py
+++ get_news_list.py
@@ -4,6 +4,32 @@ import os
 import sys
 import requests
 import anyconfig
+from datetime import datetime, timedelta
+import json
 
+# Getting url of the api to call
 config = anyconfig.load('data.yml')
-print config
+if config is None or 'baseurl' not in config:
+        print "Error parsing yaml"
+        exit(1)
+
+baseurl = config['baseurl']
+listurl = baseurl + '/api/list'
+
+# Getting timestamp of 4 days in past
+now = datetime.now()
+prev = now - timedelta(4)
+ts = (prev - datetime(1970, 1, 1)).total_seconds()
+
+# Adding the timestamp to payload
+payload = {}
+payload['timestamp'] = ts
+
+# Sending the request
+resp = requests.post(listurl, data=payload)
+if resp.status_code != 200:
+        print "Error received : " + str(resp.status_code)
+        exit(2)
+else:
+        cntnt = json.loads(resp.content)
+        print cntnt
